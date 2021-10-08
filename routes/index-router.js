2f3f232820b0678dae3a02eb17e30d59a2214aed
gaetan dumora <dumoragaetan@gmail.com>
2021-10-08 22:40:41 +0200

    correction redirect url and add about page content

--- routes/index-router.js
+++ routes/index-router.js
@@ -1,18 +1,17 @@
 import { getAccessToken } from '../authorization/auth.js'
-import { fortune } from '../templates/helpers/fortune.js'
 import { mongo } from '../data_base/Mongo.js'
-
 
 export async function showAthletes(r, h) {
     const athlete = (await mongo.getUsersInfo()).map(el => el.athlete)
     return h.view('./index.html', {
-        random_string: fortune(),
         users_info: athlete
     })
 }
 
+//when the user accept, he's redirected to /confirm with the code in the url
 export async function getAuth(r, h) {
     const { code, scope } = r.query
+    // this code is used to get the first access and refresh token
     const userToken = await getAccessToken(code)
     await mongo.upsert('users_info', { "athlete.id": userToken.athlete.id }, userToken)
     return h.view('./code.html', { code, scope, athlete: userToken.athlete })
