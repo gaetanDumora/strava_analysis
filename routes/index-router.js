1b83cdbae1c944877145fb275cf2c0c2c42c8235
gaetan dumora <dumoragaetan@gmail.com>
2021-10-15 20:13:10 +0200

    sort by name users

--- routes/index-router.js
+++ routes/index-router.js
@@ -3,7 +3,7 @@ import { mongo } from '../data_base/Mongo.js'
 
 export async function showAthletes(r, h) {
     const [{total}] = await mongo.sum("users_activity", "distance")
-    const athlete = (await mongo.getUsersInfo()).map(el => el.athlete)
+    const athlete = (await mongo.getUsersInfo()).map(el => el.athlete).sort((a, b) => (a.firstname > b.firstname) ? 1 : ((b.firstname > a.firstname) ? -1 : 0))
     return h.view('./index.html', {
         kms : (total / 1000).toLocaleString('en').replace(',',' '),
         users_info: athlete
