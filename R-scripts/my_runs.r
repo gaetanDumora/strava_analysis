909e2b46b7f68cf6c53d4d6fe1264caa08319e98
gaetan dumora <dumoragaetan@gmail.com>
2021-11-30 21:13:15 +0100

    stream data

--- R-scripts/my_runs.r
+++ R-scripts/my_runs.r
@@ -1,4 +1,6 @@
-library(dplyr)
+library("dplyr")
+library("ggplot2")
+library("factoextra")
 
 path <- "data/my_runs.csv"
 df <- read.csv(path, header = TRUE, sep = ",")
@@ -8,21 +10,27 @@ df[, c("user_id", "sex")] <- lapply(df[, c("user_id", "sex")], as.character)
 df$activity_date <- as.Date(df$activity_date)
 df[, 6:ncol(df)] <- lapply(df[, 6:ncol(df)], as.numeric)
 
+# Clusters
+coude <- fviz_nbclust(df[, 6:ncol(df)], kmeans, method = "wss")
+print(coude)
+km <- kmeans(scale(df[, 6:ncol(df)]), centers = 3)
+print(km)
 
-# coude <- factoextra::fviz_nbclust(df[, 6:ncol(df)], kmeans, method = "wss")
-# print(coude)
-# km <- kmeans(scale(df[, 6:ncol(df)]), centers = 7, nstart = 25)
-# print(km)
-
-# gkm <- factoextra::fviz_cluster(km, data = df[, 6:ncol(df)])
-# print(gkm)
+gkm <- fviz_cluster(km, data = df[, 6:ncol(df)])
+print(gkm)
 
 # print(summary(df))
 # plot(df[, 6:ncol(df)])
 year2021 <- filter(df, df$activity_date > "2021-01-01)")
+print(summary(year2021))
+# print(summary(year2021))
+# plot(year2021[,-1])
 
-
-plot(x = year2021$activity_date, y = year2021$std_speed, type = "h", col = "orange")
+print(
+    ggplot(year2021) +
+    geom_point(aes(x = calories, y = std_speed), color = "darkgreen", size = 3, alpha = 0.3)
+    )
+# print(plot(x = year2021$activity_date, y = year2021$std_speed, type = "h", col = "orange"))
 
 # # sc_df <- scale(df[, 6:ncol(df)])
 # print(head(df, 10))
--- logic/fetch-activities.js
+++ logic/fetch-activities.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { mongo } from '../data_base/Mongo.js'
 import { getUserValidToken } from './auth.js'
+import { writeFile } from 'fs/promises'
 
 const VERSION = 'v3'
 
@@ -30,6 +31,14 @@ async function browsePage(options, pageNb = 1, cb) {
     } catch (error) {
         console.error(error)
     }
+}
+
+function transpose(acc, curr, i) {
+    if (i == 0) acc = curr.map(() => [])
+    for (let j = 0; j < curr.length; j++) {
+        acc[j][i] = curr[j]
+    }
+    return acc
 }
 
 function dateToUnixTemp(stringDate) {
@@ -99,3 +108,75 @@ export async function updateAcitivities() {
     return ids.forEach(id => getUserActivities(id, sevenDaysAgo))
 }
 // await updateAcitivities()
+
+async function getStream(activittyID, userToken) {
+    try {
+        const { data: {
+            time: { data: second },
+            distance: { data: meter },
+            cadence: { data: ppm },
+            heartrate: { data: bpm },
+            altitude: { data: elevation },
+            velocity_smooth: { data: velocity },
+            grade_smooth: { data: grade },
+            temp: { data: deg }
+        } } = await axios({
+            method: 'GET',
+            url: `https://www.strava.com/api/${VERSION}/activities/${activittyID}/streams?keys=time,distance,altitude,velocity_smooth,heartrate,cadence,watts,temp,moving,grade_smooth&key_by_type=true`,
+            headers: { Authorization: `Bearer ${userToken}` }
+        })
+        const res = [
+            deg,
+            second,
+            meter,
+            ppm,
+            bpm,
+            elevation,
+            velocity,
+            grade
+        ]
+        return res.reduce(transpose, [])
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+async function streamActivities() {
+    const access = await getUserValidToken(18933919)
+    const acts = await mongo.getCollection("users_activity")
+    const thisActs = await acts.find({ "athlete.id": 18933919, "start_date": { "$gte": "2020-12-31T00:00:00Z" } }).project({ "id": 1, "_id": 0 }).toArray()
+    const ids = thisActs.map(e => e.id)
+    const stream = (await Promise.all(ids.map(id => getStream(id, access))))
+    stream.forEach((run, i) => {
+        run.forEach(async second => {
+            second.unshift(i)
+            second.push('\n')
+            const s = second.toString()
+            await writeFile('../data/stream.csv', s, { flag: "a" })
+        })
+    })
+}
+// await streamActivities()
+// await mongo.closeConnexion()
+
+const summary = [
+    { sum: 11640, mean: 13.92, std2: 6.49, std: 2.55 },
+    { sum: 1138604, mean: 1361.97, std2: 616193.54, std: 784.98 },
+    { sum: 4568335.6, mean: 5464.52, std2: 10391603.29, std: 3223.6 },
+    { sum: 75093, mean: 89.82, std2: 48.82, std: 6.99 },
+    { sum: 131518, mean: 157.32, std2: 274.2, std: 16.56 },
+    { sum: 11950, mean: 14.29, std2: 226.83, std: 15.06 },
+    { sum: 3371.09, mean: 4.03, std2: 0.62, std: 0.79 },
+    { sum: 215, mean: 0.26, std2: 8.33, std: 2.89 }
+]
+
+// const stream = await streamActivities(18933919, 6269956413)
+// const matrix = stream.reduce(transpose, [])
+// console.log(matrix)
+// await mongo.closeConnexion()
+
+const arr = [[1, 2, 3], [11, 22, 33], [111, 222, 333], [1111, 2222, 3333], [11111, 22222, 33333]]
+
+const mx = arr.reduce(transpose, [])
+
+console.log(mx)
